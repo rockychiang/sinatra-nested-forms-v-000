aef44057f74317878fad6cab65464757600f91d5
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-06 00:27:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ module FormsLab
     post "/pirates" do 
       @pirate = Pirate.new(params[:pirate])
       
-      params[:pirate][:ship]
+      params[:pirate][:ships]
       
       erb :"pirates/show"
     end
